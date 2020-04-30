1e7fc6ce36c6446fa22ef001d011e6d86c9d419e
Duã Elias <duaelias@ipamdominio.intranet>
2020-04-29 23:45:26 -0400

    Update

--- SalesWebMvcPI/Startup.cs
+++ SalesWebMvcPI/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebMvcPI
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcPIContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcPIContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcPIContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvcPI")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
