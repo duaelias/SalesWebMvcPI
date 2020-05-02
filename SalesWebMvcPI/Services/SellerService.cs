77309daae508a0f728e8dc4709b602077836fd49
Duã Elias <duaelias@ipamdominio.intranet>
2020-05-01 21:59:08 -0400

    Servico Vendedor

---
+++ SalesWebMvcPI/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using SalesWebMvcPI.Data;
+using SalesWebMvcPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvcPI.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcPIContext _context;
+        public SellerService(SalesWebMvcPIContext context)
+        {
+            _context = context;
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+    }
+}
--- SalesWebMvcPI/Startup.cs
+++ SalesWebMvcPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvcPI.Data;
+using SalesWebMvcPI.Services;
 
 namespace SalesWebMvcPI
 {
@@ -41,6 +42,7 @@ namespace SalesWebMvcPI
                         builder.MigrationsAssembly("SalesWebMvcPI")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
