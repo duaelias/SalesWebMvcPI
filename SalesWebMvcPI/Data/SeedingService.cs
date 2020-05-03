03a687819ee853c1503822c53f44aba59009e4c2
Duã Elias <duaelias@ipamdominio.intranet>
2020-05-03 18:49:06 -0400

    Criando Simples Cadastro Vendedor

--- SalesWebMvcPI/Data/SeedingService.cs
+++ SalesWebMvcPI/Data/SeedingService.cs
@@ -16,7 +16,7 @@ namespace SalesWebMvcPI.Data
         }
         public void Seed()
         {
-            if (_context.Department.Any()|| 
+            if (_context.Department.Any() ||
                 _context.Seller.Any() ||
                 _context.salesRecords.Any())
             {
@@ -74,8 +74,7 @@ namespace SalesWebMvcPI.Data
             _context.salesRecords.AddRange(
                 r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
                 r11, r12, r13, r14, r15, r16, r17, r18, r19, r20,
-                r21, r22, r23, r24, r25, r26, r27, r28, r29, r30
-            );
+                r21, r22, r23, r24, r25, r26, r27, r28, r29, r30);
 
             _context.SaveChanges();
 
