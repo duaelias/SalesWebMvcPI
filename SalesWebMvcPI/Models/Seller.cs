3eee8c9cf3dcfe444127e3440f30e77813112051
Duã Elias <duaelias@ipamdominio.intranet>
2020-05-07 23:13:45 -0400

    Validando

--- SalesWebMvcPI/Models/Seller.cs
+++ SalesWebMvcPI/Models/Seller.cs
@@ -9,18 +9,29 @@ namespace SalesWebMvcPI.Models
     public class Seller
     {
         public int Id { get; set; }
+        
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "{0} Obrigatorio")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve ser entre {2} e {1}")]
         public string Name { get; set; }
+        
+        
         [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "{0} Obrigatorio")]
+        [EmailAddress(ErrorMessage = "Entra com e-mail valido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Display(Name ="Aniversario")]
+        [Required(ErrorMessage = "{0} Obrigatorio")]
+        [Display(Name ="Aniversário")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} Obrigatorio")]
         [Display(Name = "Salario")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} o salario deve ser entre {1} to {2}")]
         public double BaseSalary { get; set; }
         
         public Department Department { get; set; }
